bb74ad654f4ca2b9ddb8bb701c8755908063553b
Bahisht-Ayaz <bahishtayaza9@gmail.com>
2025-08-28 12:20:14 +0500

    commit

--- Djangoadmin/hamara_project/tumhari_app/admin.py
+++ Djangoadmin/hamara_project/tumhari_app/admin.py
@@ -2,10 +2,11 @@ from django.contrib import admin
 from . import models
 
 # Register your models here.
-admin.site.site_header="Hamari App E-commerce"
-admin.site.site_title="E-commerce"
-admin.site.index_title="Dashboard"
-admin.site.register(models.Brand)
-admin.site.register(models.Category)
-admin.site.register(models.Product)
-admin.site.register(models.Contact)
+# admin.site.site_header="Hamari App E-commerce"
+# admin.site.site_title="E-commerce"
+# admin.site.index_title="Dashboard"
+# admin.site.register(models.Brand)
+# admin.site.register(models.Category)
+# admin.site.register(models.Product)
+# admin.site.register(models.Contact)
+admin.site.register(models.Employee)
