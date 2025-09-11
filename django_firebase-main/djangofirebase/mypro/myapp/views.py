ca5d6b3fd440a90d1ccbb3859a0cf8d80b8ff82e
Bahisht-Ayaz <bahishtayaza9@gmail.com>
2025-09-11 13:05:23 +0500

    commit

---
+++ django_firebase-main/djangofirebase/mypro/myapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import  render, redirect
+from mypro.firebase_config import db
+from django.contrib import messages
+
+def Contacts(request):
+    if request.method=="POST":
+        a = request.POST.get("name")
+        b = request.POST.get("email")
+        c = request.POST.get("message")
+
+        db.collection("contact").add({
+            "Name": a,
+            "Email" :b,
+            "Msg": c
+        })
+        messages.success(request,"query has been submitted")
+        return redirect("con")
+
+    return render(request,"myapp/Contact.html")
+
+def ShowData(request):
+    mydata = db.collection("contact").stream()
+    contact=[]
+    for a in mydata:
+       convert_dict =  a.to_dict()
+       contact.append(convert_dict)
+    return render(request,"myapp/Showdata.html",{"con":contact})
