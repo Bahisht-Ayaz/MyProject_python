a26fa8878968bf6b0a3623b1d0bf88494661bef3
Bahisht-Ayaz <bahishtayaza9@gmail.com>
2025-10-02 12:58:33 +0500

    commit

--- Linearregression/main.py
+++ Linearregression/main.py
@@ -1,3 +1,28 @@
+# import pandas as p
+# import matplotlib.pyplot as map
+# from sklearn.linear_model import LinearRegression
+
+# # (pip install openpyxl)
+
+# # pip install numpy scipy pandas scikit-learn matplotlib
+# mydata = p.read_excel("LRExample.xlsx")
+# x=mydata[["Hours Studied (X)"]]
+# y=mydata["Marks (Y)"]
+
+# # null hatane k liye
+# x=x.dropna()
+# y=y.loc[x.index]
+
+# model = LinearRegression()
+# model.fit(x,y)
+
+# # y= mx + b
+# print(f"m value = {model.coef_[0]:.2f}")
+# print(f"b value = {model.intercept_:.2f}")
+
+# user_value=float(input("Enter Value : "))
+# predict_value = model.predict([[user_value]])
+# print(f"On {user_value} this study hour\nScore will be {predict_value[0]:.2f}")
 import pandas as p
 import matplotlib.pyplot as map
 from sklearn.linear_model import LinearRegression
@@ -5,9 +30,9 @@ from sklearn.linear_model import LinearRegression
 # (pip install openpyxl)
 
 # pip install numpy scipy pandas scikit-learn matplotlib
-mydata = p.read_excel("LRExample.xlsx")
-x=mydata[["Hours Studied (X)"]]
-y=mydata["Marks (Y)"]
+mydata = p.read_csv("Salary_dataset.csv")
+x=mydata[["YearsExperience"]]
+y=mydata["Salary"]
 
 # null hatane k liye
 x=x.dropna()
@@ -20,6 +45,6 @@ model.fit(x,y)
 print(f"m value = {model.coef_[0]:.2f}")
 print(f"b value = {model.intercept_:.2f}")
 
-user_value=float(input("Enter Value : "))
+user_value=float(input("Enter Years Experience : "))
 predict_value = model.predict([[user_value]])
-print(f"On {user_value} this study hour\nScore will be {predict_value[0]:.2f}")
+print(f"On {user_value} this Years Experience\nSalary will be {predict_value[0]:.2f}")
